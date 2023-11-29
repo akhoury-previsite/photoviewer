08376b5caecf507d1e19da36a2d8727e2d491596
Alexandre Khoury <akhoury@previsite.com>
2023-11-29 10:32:24 +0200

    Fixes

--- source/PhotoViewer.ts
+++ source/PhotoViewer.ts
@@ -13,29 +13,29 @@ interface IPhotoViewerOptions {
     };
 }
 
-class PhotoViewer {
+// class PhotoViewer {
 
-    public static show(url: string, title: string = '', options: IPhotoViewerOptions = {
-        share: false,
-        closeButton: true,
-        copyToReference: false,
-        headers: '',
-        piccasoOptions: {
-            fit: true,
-            centerInside: true,
-            centerCrop: false
-        }
-    }) {
-        if (url && url.trim() === '') {
-            // Do nothing
-            return;
-        }
+//     public static show(url: string, title: string = '', options: IPhotoViewerOptions = {
+//         share: false,
+//         closeButton: true,
+//         copyToReference: false,
+//         headers: '',
+//         piccasoOptions: {
+//             fit: true,
+//             centerInside: true,
+//             centerCrop: false
+//         }
+//     }) {
+//         if (url && url.trim() === '') {
+//             // Do nothing
+//             return;
+//         }
 
-        const args = [url, title, options.share, options.closeButton, options.copyToReference, options.headers, options.piccasoOptions];
+//         const args = [url, title, options.share, options.closeButton, options.copyToReference, options.headers, options.piccasoOptions];
 
-        exec(() => { }, () => { }, 'PhotoViewer', 'show', args);
-    }
-}
+//         exec(() => { }, () => { }, 'PhotoViewer', 'show', args);
+//     }
+// }
 
 // @ts-ignore
-module.exports = PhotoViewer;
+// module.exports = PhotoViewer;
